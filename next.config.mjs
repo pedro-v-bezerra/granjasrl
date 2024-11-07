70264907771994f6781f869f02d4c130c092704c
Pedro Lima <pedrolima38@hotmail.com>
2024-11-07 18:10:11 -0300

    v.1.5

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true, // Enables React Strict Mode for development
+  images: {
+    domains: ['example.com'], // Allows images from external domains
+  },
+};
 
 export default nextConfig;
